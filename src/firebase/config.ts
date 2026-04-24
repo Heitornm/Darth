93823d5a2a6be045682d5e0f1e1437a3fb4ee20f
Heitor Nascimento Martins <heitornmartins@gmail.com>
2026-04-24 19:51:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7469515676-81b43",
+  "appId": "1:105716199273:web:556bc9343af3db2210d7f8",
+  "apiKey": "AIzaSyAxt_GsSsaqbdtiRev5dfH7vzxuomRkmu4",
+  "authDomain": "studio-7469515676-81b43.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "105716199273"
+};
